ecdbe82777cd4c11f8d7f926cecd2b0104a1f1e8
Lily Hung <janelily8871@gmail.com>
2018-10-04 17:12:58 +0800

    10 AC

--- 10.cpp
+++ 10.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 vector<vector<int> > adj;
 int color[1005];
-int ans1, ans2, ans, cnt;
+int ans1, ans2;
+vector<int> b1, b2;
+int dp[1005][1005];
 
 void bfs(int x){
     color[x] = 1;
@@ -37,8 +39,10 @@ int main(){
         cin >> n >> m;
         adj.clear();
         adj.resize(n+1);
+        b1.clear(); b1.push_back(0);
+        b2.clear(); b2.push_back(0);
         memset(color, INF, sizeof(color));
-        ans1 = ans2 = ans = cnt = 0;
+        
         for(int i = 0; i < m; i++){
             cin >> a >> b;
             adj[a].push_back(b);
@@ -48,20 +52,32 @@ int main(){
             if(color[i] == INF){
                 ans1 = ans2 = 0;
                 bfs(i);
+                b1.push_back(ans1);
+                b2.push_back(ans2);
                 if(ans1 < 0) break;
-                if(ans1 > ans2) ans += ans1, cnt++;
-                else if(ans2 > ans1) ans += ans2, cnt++;
-                else ans += ans1; 
             }
         }
-        if(ans1 < 0) cout << -1 << '\n';
-        else{
-            if(cnt % 2) ans -= (cnt-1)/2;
-            else ans -= cnt/2;
-            n -= ans + ans - 1;
-            if(n%2) cout << ans + n/2 << '\n';
-            else cout << ans + (n+1)/2 << '\n';
+
+        if(ans1 < 0){
+            cout << -1 << '\n';
+            continue;
         }
+
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+        for(int i = 1; i < b1.size(); i++){
+            for(int j = 1000; j >= b1[i]; j--){
+                if(dp[i-1][j-b1[i]]) dp[i][j] = 1;
+            }
+            for(int j = 1000; j >= b2[i]; j --){
+                if(dp[i-1][j-b2[i]]) dp[i][j] = 1;
+            }
+        }
+
+        int i = (n + 1) / 2;
+        while(!dp[b1.size()-1][i]) i ++;
+
+        cout << i << '\n';
     }
     return 0;
 }
